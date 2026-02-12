fa5f3d51e2b751167d3662bdf17a1cce3f4f5d8e
LUCAS HENRIQUE DE OLIVEIRA <henrique.oliveira03@escola.pr.gov.br>
2026-02-11 21:18:20 -0300

    agora ta certo nao é possivel

---
+++ explicado.js
@@ -0,0 +1,13 @@
+// Definine os caracteres
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz'; // Corrigi a ordem alfabética
+const numeros = '0123456789';
+const simbolos = '!@%*?';
+
+// pode ser utilizado na:
+//validação de formulários;
+//cupons de descontos;
+//Captchas;
+//Tokens de segurança;
+//IDs únicos (nickname por exemplo);
+//geração de seeds aleatórias.
